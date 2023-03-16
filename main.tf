d8f4514f107ba32913b64bb63f89e3e5b1ce9615
MohamedFarahatEA <mohamed.farahat@emiratesauction.ae>
2023-03-16 12:32:29 +0400

    terra

---
+++ main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {
+
+  }
+}
+
+## Create a Resource Group for Storage
+resource "azurerm_resource_group" "tf_rg_sampleapi" {
+  location = "uaenorth"
+  name     = "mohamedfarhattfrg"
+}
+
+## create the container group
+resource "azurerm_container_group" "tf_cg_sampleapi" {
+  name                = "cg_sampleapi"
+  location            = azurerm_resource_group.tf_rg_sampleapi.location
+  resource_group_name = azurerm_resource_group.tf_rg_sampleapi.name
+
+  ip_address_type = "public"
+  dns_name_label  = "sampleapitf"
+  os_type         = "Linux"
+
+
+  container {
+    name = "sampleapi"
+    image = "mohamedfarhat01/sampleapi"
+    cpu = "1"
+    memory = "1"
+
+    ports {
+      port = 80
+      protocol = "TCP"
+    }
+  }
+
+}
